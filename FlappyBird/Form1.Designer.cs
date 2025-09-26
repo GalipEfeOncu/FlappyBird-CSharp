72ab1915d3581c8d629514bdadaeb89a5ec92908
Galip Efe Öncü <galipefe75@gmail.com>
2025-09-26 11:40:05 +0300

    Add files via upload

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBird = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureGround = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureGround)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBird
+            // 
+            this.pictureBird.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBird.Image = global::FlappyBird.Properties.Resources.yellowbird_midflap;
+            this.pictureBird.Location = new System.Drawing.Point(236, 198);
+            this.pictureBird.Name = "pictureBird";
+            this.pictureBird.Size = new System.Drawing.Size(68, 48);
+            this.pictureBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBird.TabIndex = 0;
+            this.pictureBird.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 5;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // pictureGround
+            // 
+            this.pictureGround.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureGround.Image = global::FlappyBird.Properties.Resources._base;
+            this.pictureGround.Location = new System.Drawing.Point(0, 489);
+            this.pictureGround.Name = "pictureGround";
+            this.pictureGround.Size = new System.Drawing.Size(1014, 125);
+            this.pictureGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureGround.TabIndex = 1;
+            this.pictureGround.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(1014, 614);
+            this.Controls.Add(this.pictureGround);
+            this.Controls.Add(this.pictureBird);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureGround)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBird;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox pictureGround;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        // Player physics
+        private int velocity = 0;
+        private const int gravity = 1;
+        private const int jumpPower = -13;
+        private const int maxVelocity = 1;
+
+        // Pipe settings
+        private const int pipeWidth = 75;
+        private const int pipeSlidingSpeed = 3;
+        private const int pipeSpawnInterval = 200;
+        private const int minGap = 150;
+        private const int maxGap = 250;
+        private const int minTopPipeHeight = 150;
+        private const int maxTopPipeHeight = 300;
+        private int pipeSpawnCounter = 0;
+        private readonly List<PictureBox> pipes = new List<PictureBox>();
+
+        // Form settings
+        private const int formWidth = 1280;
+        private const int formHeight = 720;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameTimer.Tick += gameTimer_Tick;
+            gameTimer.Start();
+            Form form = this;
+            form.Width = formWidth;
+            form.Height = formHeight;
+            form.MinimizeBox = false;
+            form.FormBorderStyle = FormBorderStyle.FixedSingle;
+            form.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            velocity += gravity;
+            pictureBird.Top += velocity;
+
+            if (pictureBird.Top < 0) pictureBird.Top = 0;
+            if (pictureBird.Bottom > this.Height) pictureBird.Top = this.Height - pictureBird.Height;
+
+            if (velocity > maxVelocity) velocity = maxVelocity;
+            if (velocity < jumpPower) velocity = jumpPower;
+
+            //pipe kodları
+            pipeSpawnCounter++;
+            if (pipeSpawnCounter >= pipeSpawnInterval)
+            {
+                CreatePipe();
+                pipeSpawnCounter = 0;
+            }
+
+            foreach (var pipe in pipes)
+            {
+                pipe.Left -= pipeSlidingSpeed;
+                if (pipe.Right < 0)
+                {
+                    this.Controls.Remove(pipe);
+                    pipes.Remove(pipe);
+                    break;
+                }
+            }
+
+            //Collider kodları
+            foreach (var pipe in pipes)
+            {
+                if (pictureBird.Bounds.IntersectsWith(pipe.Bounds))
+                {
+                    gameTimer.Stop();
+                    MessageBox.Show("Game Over");
+                }
+            }
+
+            if (pictureBird.Top < 0 || pictureBird.Bottom > this.Height)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Game Over");
+            }
+
+            if (pictureBird.Bounds.IntersectsWith(pictureGround.Bounds))
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Game Over");
+            }
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                velocity = jumpPower;
+        }
+
+        private void CreatePipe()
+        {
+            Random rnd = new Random();
+            int gap = rnd.Next(minGap, maxGap);
+            int pipeHeight = rnd.Next(minTopPipeHeight, maxTopPipeHeight);
+
+            PictureBox topPipe = new PictureBox();
+            topPipe.Image = Properties.Resources.pipe_green1;
+            topPipe.SizeMode = PictureBoxSizeMode.StretchImage;
+            topPipe.Width = pipeWidth;
+            topPipe.Height = pipeHeight;
+            topPipe.Top = 0;
+            topPipe.Left = this.Width;
+            this.Controls.Add(topPipe);
+            pipes.Add(topPipe);
+
+            PictureBox bottomPipe = new PictureBox();
+            bottomPipe.Image = Properties.Resources.pipe_green;
+            bottomPipe.SizeMode = PictureBoxSizeMode.StretchImage;
+            bottomPipe.Width = pipeWidth;
+            bottomPipe.Height = this.Height - pipeHeight - gap;
+            bottomPipe.Top = pipeHeight + gap;
+            bottomPipe.Left = this.Width;
+            this.Controls.Add(bottomPipe);
+            pipes.Add(bottomPipe);
+        }
+    }
+}
